fe15d6bbbd1fb4d497f1e8523c4cc5eef22d91a1
Eric M <itsjusteza@gmail.com>
2021-01-14 00:12:33 +1000

    Updated index page to contain a demo of the shortener.

---
+++ src/UrlShortener/wwwroot/js/index.js
@@ -0,0 +1,33 @@
+﻿function shorten() {
+    const url = document.getElementById("input").value
+
+    fetch(`/shorten`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(url)
+    })
+    .then(async (res) => {
+        const text = await res.text()
+        if (res.ok) {
+            updateResult(text)
+            return
+        }
+        throw new Error(text)
+    })
+    .catch(err => handleError(err))
+}
+
+function updateResult(result) {
+    const elem = document.getElementById("result")
+    const link = window.location.href + result
+    elem.innerText = link
+    elem.setAttribute("href", link)
+}
+
+function handleError(error) {
+    const elem = document.getElementById("result")
+    elem.innerText = error
+    elem.removeAttribute("href")
+}
