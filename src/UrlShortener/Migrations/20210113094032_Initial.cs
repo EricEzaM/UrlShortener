0a8d7a057646c4d7c525e0c53457ca8846bf39cb
Eric M <itsjusteza@gmail.com>
2021-01-13 19:47:06 +1000

    Created first migration, added docker-compose to start up database

---
+++ src/UrlShortener/Migrations/20210113094032_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace UrlShortener.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "shortened_urls",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    url = table.Column<string>(type: "text", nullable: false),
+                    key = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_shortened_urls", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_shortened_urls_key",
+                table: "shortened_urls",
+                column: "key",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "shortened_urls");
+        }
+    }
+}
